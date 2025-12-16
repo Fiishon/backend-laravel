fa5ca9d820bf0f9ae49a9329ed55f3a6e2205503
Fishon <electrofashioon03@gmail.com>
2025-12-16 01:42:49 -0600

    archivo docker

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/crear-admin-secreto', function () {
+Route::get('/crear-admin', function () {
     // 1. Verificar si ya existe
     if (User::where('correo', 'admin@expocode.com')->exists()) {
-        return "El usuario Admin YA existe.";
+        return "El usuario Admin ya existe.";
     }
 
     // 2. Crear usando TUS columnas
     $user = User::create([
-        'nombre'           => 'Super',
+        'nombre'           => 'Admin',
         'apellido_paterno' => 'Admin',
-        'apellido_materno' => 'Sistema',
+        'apellido_materno' => 'Admin',
         'correo'           => 'admin@expocode.com',
-        'contraseña'       => Hash::make('password123'), // Encriptamos la contraseña
+        'contraseña'       => Hash::make('contraseña'), // Encriptamos la contraseña
         'rol'              => 'admin', // Asegúrate que 'admin' sea el valor correcto en tu DB
     ]);
 
