e61567c59aa8ef8c7da3d8fcc2f353c8b3ed65a5
Fishon <electrofashioon03@gmail.com>
2025-12-16 01:41:25 -0600

    admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/crear-admin-secreto', function () {
+    // 1. Verificar si ya existe
+    if (User::where('correo', 'admin@expocode.com')->exists()) {
+        return "El usuario Admin YA existe.";
+    }
+
+    // 2. Crear usando TUS columnas
+    $user = User::create([
+        'nombre'           => 'Super',
+        'apellido_paterno' => 'Admin',
+        'apellido_materno' => 'Sistema',
+        'correo'           => 'admin@expocode.com',
+        'contraseña'       => Hash::make('password123'), // Encriptamos la contraseña
+        'rol'              => 'admin', // Asegúrate que 'admin' sea el valor correcto en tu DB
+    ]);
+
+    return "¡Admin creado con éxito! Correo: " . $user->correo;
 });
